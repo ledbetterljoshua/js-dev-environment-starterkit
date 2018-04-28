073969c2b4d0ed15e138274f7c6fa856710e3e7f
ledbetterljoshua <ledbetterljoshua@gmail.com>
2018-04-28 13:17:51 -0500

    set up bundle splitting, so the fetch module (and all other modules get bundled in our app separately)

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,39 @@
 import path from 'path'
 import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: 'false',
-  entry: [
-    path.resolve(__dirname, 'src/index')
-  ],
+  entry: {
+    vendor: path.resolve(__dirname, 'src/vendor'),
+    main: path.resolve(__dirname, 'src/index')
+  },
   target: 'web',
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   plugins: [
+    // create a new html file
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      },
+      inject: true
+    }),
     //no duplicate code
     new webpack.optimize.DedupePlugin(),
     //Minify
